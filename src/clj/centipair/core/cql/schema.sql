0322c224ac43732ae6fb0921a1e52ab4c925a391
Devasia Joseph <devasiajoseph@gmail.com>
2014-12-08 20:43:23 +0530

    Added box schema

--- src/clj/centipair/core/cql/schema.sql
+++ src/clj/centipair/core/cql/schema.sql
@@ -2,7 +2,7 @@
 
 /*Key space*/
 /*Use your keyspace name*/
-DROP KEYSPACE core;
+DROP KEYSPACE newslamp;
 CREATE KEYSPACE core WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
 USE core;
 
@@ -15,6 +15,13 @@ CREATE TABLE user_login_username(username text PRIMARY KEY, user_id timeuuid);
 CREATE TABLE user_login_email(email text PRIMARY KEY, user_id timeuuid);
 CREATE TABLE user_session (auth_token text PRIMARY KEY, session_expire_time timestamp, user_id timeuuid);
 CREATE TABLE user_session_index (user_id timeuuid, auth_token text, PRIMARY KEY(user_id, auth_token));
-
 CREATE TABLE user_account_registration(registration_key timeuuid PRIMARY KEY, user_id timeuuid);
 CREATE TABLE password_reset(password_reset_key timeuuid PRIMARY KEY, user_id timeuuid, expiry timestamp);
+
+CREATE TABLE box(box_id timeuuid PRIMARY KEY, box_name text);
+CREATE TABLE domain_box(domain_name text PRIMARY KEY, box_id timeuuid);
+CREATE TABLE box_page(box_page text PRIMARY KEY, box_id timeuuid);
+CREATE TABLE box_admin(user_id timeuuid PRIMARY KEY, box_id timeuuid);
+CREATE TABLE page(page_id timeuuid PRIMARY KEY, title text, content text, last_edited timestamp);
+CREATE TABLE url(box_id timeuuid, url text, page_id timeuuid, PRIMARY KEY(box_id, url));
+CREATE TABLE tag(tag_name text PRIMARY KEY, box_id_id timeuuid, url text);

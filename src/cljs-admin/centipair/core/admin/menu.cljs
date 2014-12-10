4f7bd25172ebdc270e649826cdd5cd6fa391b603
Devasia Joseph <devasiajoseph@gmail.com>
2014-12-10 17:01:27 +0530

    Admin Menu respond

--- src/cljs-admin/centipair/core/admin/menu.cljs
+++ src/cljs-admin/centipair/core/admin/menu.cljs
@@ -10,11 +10,28 @@
                        {:label "Settings" :url "/settings" :id "settings" :active false}]))
 
 
+(defn title-component [id]
+  (reagent/render-component
+   [:h3 (:label (first (filter (fn [each] (= (:id each) id)) @admin-menu)))]
+   (. js/document (getElementById "admin-title"))))
+
+
+(defn deactivate [id item]
+  (if (= id (:id item))
+    (assoc item :active true)
+    (assoc item :active false)))
+
+
+(defn activate-side-menu-item [id]
+  (title-component id)
+  (reset! admin-menu (into [] (map (partial deactivate id) @admin-menu))))
+
+
 (defn menu-component [com-data]
   [:ul
    (map 
     (fn [each] 
-      [:li  {:key (:id each)} 
+      [:li  {:key (:id each) :class (if (:active each) "menu-item-divided pure-menu-selected" "")} 
        [:a {:href (str "#" (:url each)) :key (:id each)} (:label each)]]) 
     @com-data)])
 
@@ -29,10 +46,12 @@
 
 
 (defroute dashboard "/dashboard" []
+  (activate-side-menu-item "dashboard")
   (js/console.log "Dashboard"))
 
 
 (defroute dashboard "/settings" []
+  (activate-side-menu-item "settings")
   (js/console.log "Settings"))
 
 
